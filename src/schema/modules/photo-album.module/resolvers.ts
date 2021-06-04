8bffdf75db389a9b837997fa8d8ce35da1f2e2f8
AndriyMazuryk <andriy.mzrk@gmail.com>
2021-06-04 22:37:41 +0300

    ✨ Feature: add createPhotoAlbum mutation

--- src/schema/modules/photo-album.module/resolvers.ts
+++ src/schema/modules/photo-album.module/resolvers.ts
@@ -1,6 +1,38 @@
 import { IResolvers } from 'graphql-tools';
+import { PhotoAlbum, User } from '../../../entity';
+import { response } from '../../../utils';
 
 export const resolvers: IResolvers = {
   Query: {},
-  Mutation: {},
+  Mutation: {
+    createPhotoAlbum: async (_, { title, description }, { req }) => {
+      if (!req.userId) {
+        return response(false, 'The user is not authorized!');
+      }
+
+      const user = await User.findOne({
+        where: { id: req.userId },
+        relations: ['photoAlbums'],
+      });
+      if (!user) {
+        return response(false, 'There is no user with this ID!');
+      }
+
+      const photoAlbum = await PhotoAlbum.create({
+        title,
+        description,
+      }).save();
+      if (!photoAlbum) {
+        return response(false, "Photo album hasn't been created!");
+      }
+
+      user.photoAlbums.push(photoAlbum);
+      await user.save();
+
+      return response(
+        true,
+        `Photo album ${photoAlbum.title} has been created!`
+      );
+    },
+  },
 };
