63131cbc5541a0d7e58f53ac8488fde0867fdc34
AndriyMazuryk <andriy.mzrk@gmail.com>
2021-06-10 00:31:37 +0300

    ✨ Feature: add getMessagesByUserId query

--- src/schema/modules/message.module/resolvers.ts
+++ src/schema/modules/message.module/resolvers.ts
@@ -3,6 +3,63 @@ import { Message, User } from '../../../entity';
 import { response, message } from '../../../utils';
 
 export const resolvers: IResolvers = {
+  Query: {
+    getMessagesByUserId: async (_, { userId }, { req }) => {
+      if (!req.userId) {
+        return null;
+      }
+
+      const user = await User.findOne({
+        where: { id: req.userId },
+        relations: ['avatar'],
+      });
+      if (!user) {
+        return null;
+      }
+
+      const interlocutor = await User.findOne({
+        where: { id: userId },
+        relations: ['avatar'],
+      });
+      if (!interlocutor) {
+        return null;
+      }
+
+      let sentMessages;
+      try {
+        sentMessages = await Message.find({
+          where: {
+            author: user,
+            recipient: interlocutor,
+          },
+          relations: ['author', 'recipient'],
+        });
+      } catch (error) {
+        console.log(error);
+        return null;
+      }
+
+      let receivedMessages;
+      try {
+        receivedMessages = await Message.find({
+          where: {
+            author: interlocutor,
+            recipient: user,
+          },
+          relations: ['author', 'recipient'],
+        });
+      } catch (error) {
+        console.log(error);
+        return null;
+      }
+
+      const messages = [...sentMessages, ...receivedMessages].sort(
+        (a, b) => b.id - a.id
+      );
+
+      return messages;
+    },
+  },
   Mutation: {
     sendMessage: async (_, { recipientId, text }, { req }) => {
       if (!req.userId) {
